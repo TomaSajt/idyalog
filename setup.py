9199051cec442488326e864d98338751891b3362
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-06-18 00:37:51 +0200

    Bump

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name='idyalog',
       version='0.1.0',
       description='A Dyalog APL kernel for Jupyter',
-      author='TomaSajt',
       license='MIT',
       url="https://github.com/TomaSajt/idyalog",
       install_requires=['notebook >= 4.0'],
